bbefbceeba575650ac16383e3ed1460f7cd227b1
barirafatima <barirafatima.q@gmail.com>
2024-12-02 11:03:45 +0500

    my-react-project

---
+++ my-react-lab/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import greeting from './Components/greeting'
+function App() {
+  return (
+    <div className="App">
+    <Greeting/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-react-lab/src/Components/greeting.jsx
@@ -0,0 +1,8 @@
+let name="Barira Fatima";
+function greeting() {
+    return(
+        <h1>Hello, {name} WElcome to react!</h1>
+      
+    )
+}
+export default Greeting;
